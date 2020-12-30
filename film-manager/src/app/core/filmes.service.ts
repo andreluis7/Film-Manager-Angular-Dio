3979cc80f1be56304f761cdb4806e12761c1460f
Andre Luis <adrluis7@gmail.com>
2020-12-29 21:02:19 -0300

    Filtro e ordenação/listagem

--- film-manager/src/app/core/filmes.service.ts
+++ film-manager/src/app/core/filmes.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Filme } from '../shared/models/filme';
+import { ConfigPrams } from '../shared/models/config-prams';
+import { ConfigParamsService } from './config-params.service';
 
 const url = 'http://localhost:3000/filmes/';
 
@@ -10,9 +12,15 @@ const url = 'http://localhost:3000/filmes/';
 })
 export class FilmesService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private configService: ConfigParamsService) { }
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
+  }
+
+  listar(config: ConfigPrams): Observable<Filme[]> {
+    const configPrams = this.configService.configurarParametros(config);
+    return this.http.get<Filme[]>(url, {params: configPrams});
   }
 }
